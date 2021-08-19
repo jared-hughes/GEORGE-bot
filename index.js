b24c332ae985bf5dd295b013f973e86dfa8cf3e7
Jared Hughes <jahughes241@gmail.com>
2021-08-19 15:43:15 -0700

    Initial bot

---
+++ index.js
@@ -0,0 +1,17 @@
+const { Client } = require("discord.js");
+const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+const prefix = "]GEORGE";
+
+client.on("messageCreate", (msg) => {
+  if (msg.author.bot) return;
+  if (!msg.content.startsWith(prefix)) return;
+  msg.reply("I am summoned\n" + msg.content.slice(prefix.length));
+  console.log(msg.content);
+});
+
+client.login(process.env.DISCORD_TOKEN);
